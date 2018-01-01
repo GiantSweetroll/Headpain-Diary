8beeabeaba90695ada384b07ceee9b9244329408
GiantSweetroll <33752872+GiantSweetroll@users.noreply.github.com>
2018-01-01 11:00:47 +0700

    Updates on Table Panel

--- src/diary/gui/table/TablePanel.java
+++ src/diary/gui/table/TablePanel.java
@@ -1,6 +1,10 @@
 package diary.gui.table;
 
+import javax.swing.JComboBox;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+import diary.gui.DateRangePanel;
 
 public class TablePanel extends JPanel
 {
@@ -10,5 +14,9 @@ public class TablePanel extends JPanel
 	 */
 	private static final long serialVersionUID = -6429132267457024269L;
 	
-
+	private JPanel panelTop, panelTopLeft, panelBelowLeft, panelBelowCenter;
+	private DateRangePanel panelDateRange;
+	private JComboBox<String> comboFilter;
+	private JTextField tfFilter;
+	
 }
