0991fd197de4c3b8cb07c8ae1b51f8dc247d59a0
GiantSweetroll <33750894+GiantSweetroll@users.noreply.github.com>
2018-02-03 15:57:17 +0700

    Added new class
    In preparation for patient data entering

---
+++ src/diary/patientdata/PatientDataForm.java
@@ -0,0 +1,15 @@
+package diary.patientdata;
+
+import javax.swing.JPanel;
+
+public class PatientDataForm extends JPanel
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2197578997400146750L;
+	
+	
+	
+}
