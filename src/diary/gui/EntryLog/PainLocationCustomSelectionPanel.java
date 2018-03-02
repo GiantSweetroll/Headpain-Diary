2bbab9d95d51d6d731b922e78e9d209b7cc06d42
GiantSweetroll <33752872+GiantSweetroll@users.noreply.github.com>
2018-03-02 15:12:09 +0700

    Created class for custom pain location

---
+++ src/diary/gui/EntryLog/PainLocationCustomSelectionPanel.java
@@ -0,0 +1,21 @@
+package diary.gui.EntryLog;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class PainLocationCustomSelectionPanel extends JPanel
+{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6306460492486489780L;
+	
+	private JLabel image;
+	
+	//Constructors
+	public PainLocationCustomSelectionPanel()
+	{
+		
+	}
+}
